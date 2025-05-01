74b1506ff397a053947000f8a8f95301a7307e38
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:10:10 +0300

    LoggingView.xaml.cs

---
+++ SiteScraper/Logging/Views/LoggingView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using OfflineWebsiteViewer.Logging.ViewModels;
+
+namespace OfflineWebsiteViewer.Logging.Views
+{
+    /// <summary>
+    /// Interaction logic for LoggingView.xaml
+    /// </summary>
+    public partial class LoggingView : Window
+    {
+        public static LoggingView Instance = new LoggingView();
+
+        public LoggingView()
+        {
+            InitializeComponent();
+            this.DataContext = new LoggingViewModel();
+        }
+    }
+}
