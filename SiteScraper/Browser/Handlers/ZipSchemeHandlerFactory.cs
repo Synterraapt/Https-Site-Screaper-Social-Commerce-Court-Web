413c9aae9f87a727badcf4af0a4ea4271eb8a55f
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:47 +0300

    ZipSchemeHandlerFactory.cs

---
+++ SiteScraper/Browser/Handlers/ZipSchemeHandlerFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using CefSharp;
+using OfflineWebsiteViewer.Project;
+
+namespace OfflineWebsiteViewer.Browser.Handlers
+{
+    class ZipSchemeHandlerFactory : ISchemeHandlerFactory
+    {
+        public static string SchemeName = "zip";
+
+        public IResourceHandler Create(IBrowser browser, IFrame frame, string schemeName, IRequest request)
+        {
+            var project = (Application.Current as App)?.Project as ArchiveProject;
+
+            if(project?.Archive != null)
+                return new ZipResourceHandler(project.Archive);
+            return new ResourceHandler();
+        }
+    }
+}
