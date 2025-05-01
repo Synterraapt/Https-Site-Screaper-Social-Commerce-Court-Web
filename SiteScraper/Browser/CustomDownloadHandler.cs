8cd52c89f1bc4b1b481699da6146b953b288e899
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:09:39 +0300

    CustomDownloadHandler.cs

---
+++ SiteScraper/Browser/CustomDownloadHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using CefSharp;
+using NLog;
+
+namespace OfflineWebsiteViewer.Browser
+{
+    public class CustomDownloadHandler : IDownloadHandler
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public void OnBeforeDownload(IBrowser browser, DownloadItem downloadItem, IBeforeDownloadCallback callback)
+        {
+            if (!callback.IsDisposed)
+            {
+                using (callback)
+                {
+                    var project = (Application.Current as App)?.Project;
+                    if(project != null)
+                    { 
+                        try
+                        {
+                            var fileUri = new Uri(downloadItem.Url);
+                            Logger.Info($"Trying to opening file {fileUri.LocalPath}");
+                            project.OpenFile(fileUri.LocalPath);
+                        }
+                        catch (Exception ex)
+                        {
+                            Logger.Warn(ex, "Failed to open file directly, falling back to show save dialog");
+                            callback.Continue(downloadItem.SuggestedFileName, showDialog: true);
+                        }
+                    }
+                    else
+                    {
+                        callback.Continue(downloadItem.SuggestedFileName, showDialog: true);
+                    }
+                }
+            }
+        }
+
+        public void OnDownloadUpdated(IBrowser browser, DownloadItem downloadItem, IDownloadItemCallback callback)
+        {
+        }
+    }
+}
