d5837aac4b7233912edc88b407e25504a3c1702e
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 02:10:24 +0300

    IProject.cs

---
+++ SiteScraper/Project/IProject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OfflineWebsiteViewer.Search;
+
+namespace OfflineWebsiteViewer.Project
+{
+    public interface IProject : IDisposable
+    {
+        string Name { get; }
+        string ProjectPath { get; }
+        bool IsArchive { get; }
+        string IndexFile { get; }
+        string IndexUrl { get; }
+        HtmlFileIndex SearchIndex { get; }
+        bool Open();
+        string GetUrl(string file);
+        void CreateIndex(Action callback);
+        void OpenFile(string file);
+    }
+}
